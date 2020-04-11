75db1592df97e997629ab70fdd74e67ea057e86c
root <root@localhost.localdomain>
2020-04-12 00:31:46 +0600

    routing of user

--- src/app/landing/user/user-routing.module.ts
+++ src/app/landing/user/user-routing.module.ts
@@ -2,8 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UserComponent } from './user.component';
+import {FeedsComponent} from './feeds/feeds.component';
+import {ProfileComponent} from './profile/profile.component';
+import {FeaturesComponent} from './features/features.component';
+import {BookstoreComponent} from './bookstore/bookstore.component';
 
-const routes: Routes = [{ path: '', component: UserComponent }];
+const routes: Routes = [
+  { path: '', component: FeedsComponent },
+  {path: 'profile', component: ProfileComponent},
+  {path: 'feature', component: FeaturesComponent},
+  {path: 'store', component: BookstoreComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
