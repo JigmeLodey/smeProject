c266c42f70cf30fdfcec3f178db3961fb19d0511
root <root@localhost.localdomain>
2020-04-11 19:54:25 +0600

    added new components in user

--- src/app/landing/user/user.module.ts
+++ src/app/landing/user/user.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
+import { FeedsComponent } from './feeds/feeds.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BookstoreComponent } from './bookstore/bookstore.component';
+import { FeaturesComponent } from './features/features.component';
 
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [UserComponent, FeedsComponent, ProfileComponent, BookstoreComponent, FeaturesComponent],
   imports: [
     CommonModule,
     UserRoutingModule
